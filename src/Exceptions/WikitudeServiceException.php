2b42c54a68f744620a79a9fdbe52401396e10446
Jose Suarez <chua.jose@gmail.com>
2017-02-23 15:33:49 +0100

    Laravel Wikitude

--- src/Exceptions/WikitudeServiceException.php
+++ src/Exceptions/WikitudeServiceException.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of jwt-auth.
+ * This file is part of laravel-wikitude.
  *
- * (c) Sean Tymon <tymon148@gmail.com>
+ * (c) Jose Suárez <chua.jose@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
